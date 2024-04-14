4053c6d8aef7558aed3a7aababdfc05596490bba
Adrian <adrmalec@proton.me>
2024-04-14 12:49:07 +0200

    chore:zadanie4

--- Company.java
+++ Company.java
@@ -1,28 +1,32 @@
 
 public class Company {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
+        Employee[] employees = new Employee[7];
 
         employees[0] = new Manager("Adam Kowalski", 4000);
         employees[1] = new Worker("Wojciech Szczesny", 5000);
         employees[2] = new Worker("Robert Lewandowski", 7000);
         employees[3] = new Worker("Jan Wolski", 2000500);
         employees[4] = new Worker("Kuba Blaszczykowski", 300000);
+        employees[5] = new Manager("Jakub Kiwior", 80000); 
+        employees[6] = new Worker("Jack Grealish", 60000); 
 
-        int nonmanagercount = 0;
+        // Zwiększenie pensji wszystkich pracowników o 500
         for (Employee emp : employees) {
-            if (!(emp instanceof Manager)) {
-                nonmanagercount++;
+            double newSalary = emp.getSalary() + 500;
+            emp.setSalary(newSalary);
+        }
+
+        for (Employee emp : employees) {
+            if (emp instanceof Manager) {
+                ((Manager) emp).setNumberOfSubordinates(5);
+                emp.setSalary(7500);
             }
         }
 
-        ((Manager) employees[0]).setNumberOfSubordinates(nonmanagercount);
-
-        employees[0].setSalary(7500);
-
-        System.out.println("Dane wszystkich pracowników:");
+        System.out.println("Zaktualizowane informacje o pracownikach:");
         for (Employee emp : employees) {
-            System.out.println(emp);
+            System.out.println(emp.toString());
         }
     }
 }
--- Employee.java
+++ Employee.java
@@ -20,7 +20,6 @@ public class Employee {
         this.salary = newSalary;
     }
 
-    @Override
     public String toString() {
         return "Employee: " + fullName + ", Salary: " + salary;
     }
